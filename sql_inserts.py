f2757da52034751bd7483bb494d8a58b01d499c7
Sanjee <sanjee.yogeswaran@gmail.com>
2021-12-15 09:40:51 -0600

    updated files for gcloud build

--- sql_inserts.py
+++ sql_inserts.py
@@ -23,78 +23,9 @@ async def billProcessor(billList, congressNumber, table, session):
     print(f'Processing: Congress: {congressNumber} Type: {billType}')
     for b in billList:
         try:
-            # filePath = f'congress/data/{congressNumber}/bills/{table.__tablename__}/{bill}/data.json'
-            # if os.path.exists(filePath):
-            # if False:
-            #     async with aiofiles.open(filePath) as f:
-            #         contents = await f.read()
-            #         data = ujson.loads(contents)
-            #         billnumber = data['number']
-            #         billtype = data['bill_type']
-            #         introduceddate = data['introduced_at']
-            #         congress = data['congress']
-            #
-            #         ## committee code
-            #         committees = data['committees']
-            #         committeelist = []
-            #         try:
-            #             for com in committees:
-            #                 committee = data['committee']
-            #                 committeelist.append(
-            #                     {'committee': committee})
-            #         except:
-            #             pass
-            #
-            #         try:
-            #             title = data['short_title']
-            #             if title is None:
-            #                 title = data['official_title']
-            #         except:
-            #             pass
-            #
-            #         # ignore if no summary
-            #         try:
-            #             summary = data['summary']['text']
-            #         except:
-            #             continue
-            #
-            #         actions = data['actions']
-            #         actionlist = []
-            #         for a in actions:
-            #             actionlist.append({'date': a['acted_at'], 'text': a['text'], 'type': a['type']})
-            #         actionlist.reverse()
-            #         ## sponsors code
-            #         sponsorlist = []
-            #         sponsor = data['sponsor']
-            #         if sponsor is not None:
-            #             if sponsor['title'] == 'sen':
-            #                 sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}]"
-            #             else:
-            #                 sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}-{sponsor['district']}]"
-            #         sponsorlist.append({'fullname': sponsortitle})
-            #         cosponsorlist = []
-            #         try:
-            #             cosponsors = data['cosponsors']
-            #             for sponsor in cosponsors:
-            #                 if sponsor['title'] == 'sen':
-            #                     sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}]"
-            #                 else:
-            #                     sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}-{sponsor['district']}]"
-            #             cosponsorlist.append({'fullname': sponsortitle})
-            #         except:
-            #             traceback.format_exc()
-            #         try:
-            #             status_at = data['status_at']
-            #         except:
-            #             traceback.format_exc()
-            #         sql = table(billnumber=billnumber, billtype=billtype, introduceddate=introduceddate,
-            #                     congress=congress, committees=committeelist, actions=actionlist,
-            #                     sponsors=sponsorlist, cosponsors=cosponsorlist,
-            #                     title=title, summary=summary, status_at=status_at)
-            #         session.merge(sql)
-            if os.path.exists(f'congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/fdsys_billstatus.xml'):
+            if os.path.exists(f'/congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/fdsys_billstatus.xml'):
                 # print(f'processing {table.__tablename__}/{b}/')
-                filename = f'congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/fdsys_billstatus.xml'
+                filename = f'/congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/fdsys_billstatus.xml'
                 try:
                     tree = ET.parse(filename)
                     root = tree.getroot()
@@ -171,6 +102,75 @@ async def billProcessor(billList, congressNumber, table, session):
                 except:
                     traceback.print_exc()
                     continue
+            elif os.path.exists(f'/congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/data.json'):
+                filePath = f'/congress/data/{congressNumber}/bills/{table.__tablename__}/{bill}/data.json'
+                if os.path.exists(filePath):
+                    async with aiofiles.open(filePath) as f:
+                        contents = await f.read()
+                        data = ujson.loads(contents)
+                        billnumber = data['number']
+                        billtype = data['bill_type']
+                        introduceddate = data['introduced_at']
+                        congress = data['congress']
+
+                        ## committee code
+                        committees = data['committees']
+                        committeelist = []
+                        try:
+                            for com in committees:
+                                committee = data['committee']
+                                committeelist.append(
+                                    {'committee': committee})
+                        except:
+                            pass
+
+                        try:
+                            title = data['short_title']
+                            if title is None:
+                                title = data['official_title']
+                        except:
+                            pass
+
+                        # ignore if no summary
+                        try:
+                            summary = data['summary']['text']
+                        except:
+                            continue
+
+                        actions = data['actions']
+                        actionlist = []
+                        for a in actions:
+                            actionlist.append({'date': a['acted_at'], 'text': a['text'], 'type': a['type']})
+                        actionlist.reverse()
+                        ## sponsors code
+                        sponsorlist = []
+                        sponsor = data['sponsor']
+                        if sponsor is not None:
+                            if sponsor['title'] == 'sen':
+                                sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}]"
+                            else:
+                                sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}-{sponsor['district']}]"
+                        sponsorlist.append({'fullname': sponsortitle})
+                        cosponsorlist = []
+                        try:
+                            cosponsors = data['cosponsors']
+                            for sponsor in cosponsors:
+                                if sponsor['title'] == 'sen':
+                                    sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}]"
+                                else:
+                                    sponsortitle = f"{sponsor['title']} {sponsor['name']} [{sponsor['state']}-{sponsor['district']}]"
+                            cosponsorlist.append({'fullname': sponsortitle})
+                        except:
+                            traceback.format_exc()
+                        try:
+                            status_at = data['status_at']
+                        except:
+                            traceback.format_exc()
+                        sql = table(billnumber=billnumber, billtype=billtype, introduceddate=introduceddate,
+                                    congress=congress, committees=committeelist, actions=actionlist,
+                                    sponsors=sponsorlist, cosponsors=cosponsorlist,
+                                    title=title, summary=summary, status_at=status_at)
+                        session.merge(sql)
         except:
             traceback.print_exc()
             print(f'{congressNumber}/{table.__tablename__}-{b} does not exist')
@@ -187,8 +187,11 @@ async def main():
         congressNumbers = range(108, 118)
         with Session() as session:
             for congressNumber in congressNumbers:
-                bills = os.listdir(f'congress/data/{congressNumber}/bills/{table.__tablename__}')
-                tasks.append(asyncio.ensure_future(billProcessor(bills, congressNumber, table, session)))
+                try:
+                    bills = os.listdir(f'/congress/data/{congressNumber}/bills/{table.__tablename__}')
+                    tasks.append(asyncio.ensure_future(billProcessor(bills, congressNumber, table, session)))
+                except:
+                    print(f'not bills for {congressNumber}')
             await asyncio.gather(*tasks)
             print(f'Processed: {table.__tablename__}')
 
@@ -199,8 +202,7 @@ async def main():
 
 async def update_files(update_only=False):
     print(os.listdir('.'))
-    os.system('congress/run govinfo --bulkdata=BILLSTATUS')
-    os.chdir('/')
+    os.system('/congress/run govinfo --bulkdata=BILLSTATUS')
     if update_only:
         await update()
 
@@ -209,7 +211,7 @@ async def update():
     with Session() as session:
         tasks = []
         for table in tables:
-            bills = os.listdir(f'congress/data/117/bills/{table.__tablename__}')
+            bills = os.listdir(f'/congress/data/117/bills/{table.__tablename__}')
             tasks.append(asyncio.ensure_future(billProcessor(bills, 117, table, session)))
         await asyncio.gather(*tasks)
 
